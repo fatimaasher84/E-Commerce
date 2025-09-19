4409b46b21868162a4abb546d2ed1bb93fd73f0b
fatimaasher84 <129897894+fatimaasher84@users.noreply.github.com>
2025-09-20 02:57:20 +0500

    Add files via upload

--- ecom/settings.py
+++ ecom/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = 'django-insecure-^pl336$dxf0ofp-2ir3^n34w1=@vcw$sqy15om3c!h1npwjst+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-commerce-production-6099.up.railway.app','https://e-commerce-production-6099.up.railway.app']
-CSRF_TRUSTED_ORIGINS=['e-commerce-production-6099.up.railway.app','https://e-commerce-production-6099.up.railway.app']
+ALLOWED_HOSTS = ['https://e-commerce-production-6099.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['https://e-commerce-production-6099.up.railway.app']
 
 
 # Application definition
